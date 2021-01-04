303a07c119d9073ff2853fe177f31cd55bf2041e
Omrigo312 <omrigo312@gmail.com>
2021-01-04 23:59:30 +0200

    working on responsiveness

--- client/src/components/layout/Landing.tsx
+++ client/src/components/layout/Landing.tsx
@@ -1,3 +1,17 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+
 export default function Landing() {
-  return <div className="island-background"></div>;
+  return (
+    <div className="island-background">
+      <div className="landing-buttons">
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
+        <Button variant="contained" color="primary">
+          Register
+        </Button>
+      </div>
+    </div>
+  );
 }
